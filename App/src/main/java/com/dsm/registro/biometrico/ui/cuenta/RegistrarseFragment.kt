f92b81848114de13d1b2054750b40ad850a47258
birdkron <js8215001@gmail.com>
2024-04-30 09:47:58 -0600

    Ajuste para registro de la cuenta de usuario

--- App/src/main/java/com/dsm/registro/biometrico/ui/cuenta/RegistrarseFragment.kt
+++ App/src/main/java/com/dsm/registro/biometrico/ui/cuenta/RegistrarseFragment.kt
@@ -1,6 +1,10 @@
 package com.dsm.registro.biometrico.ui.cuenta
 
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.TextUtils
 import android.util.Log
 import android.util.Patterns
@@ -17,10 +21,15 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.airbnb.lottie.LottieAnimationView
 import com.dsm.registro.biometrico.R
-import com.dsm.registro.biometrico.databinding.FragmentNotificationsBinding
+import com.dsm.registro.biometrico.databinding.FragmentRegistrarseBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
-import com.dsm.registro.biometrico.databinding.FragmentRegistrarseBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import java.io.File
+import java.io.FileInputStream
+
 
 class RegistrarseFragment : Fragment(R.layout.fragment_registrarse) {
 
@@ -33,6 +42,7 @@ class RegistrarseFragment : Fragment(R.layout.fragment_registrarse) {
     private val binding get() = _binding!!
 
     var NombreEt: EditText? = null
+    var ApellidoEt: EditText? = null
     var CorreoEt:EditText? = null
     var ContaseñaEt:EditText? = null
     var ConfirmarContraseñaEt:EditText? = null
@@ -41,17 +51,38 @@ class RegistrarseFragment : Fragment(R.layout.fragment_registrarse) {
     var BtnAgregarImagen: LottieAnimationView? = null
 
     var firebaseAuth: FirebaseAuth? = null
+    var storage: FirebaseStorage? = null
 
     var nombre = " "
+    var apellido = " "
     var correo = " "
     var password = ""
     var confirmarpassword = ""
+    var uriImagen: String? = null
 
     val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
         if (uri != null) {
+            uriImagen = context?.let { getRealPathFromURI(it,uri) }
             Log.d("PhotoPicker", "Selected URI: $uri")
         } else {
             Log.d("PhotoPicker", "No media selected")
+        }
+    }
+
+    private fun getRealPathFromURI(context: Context, contentUri: Uri): String? {
+        var cursor: Cursor? = null
+        return try {
+            val proj = arrayOf(MediaStore.Images.Media.DATA)
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null)
+            val column_index: Int = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            cursor.moveToFirst()
+//            Log.i("Real Path Imagen ->",cursor.getString(column_index))
+            cursor.getString(column_index)
+        } catch (e: Exception) {
+            Log.e("Fallo al obtener URI: ", "getRealPathFromURI Exception : $e")
+            ""
+        } finally {
+            cursor?.close()
         }
     }
 
@@ -63,6 +94,7 @@ class RegistrarseFragment : Fragment(R.layout.fragment_registrarse) {
         val root: View = binding.root
 
         NombreEt = binding.NombreEt
+        ApellidoEt = binding.ApellidoEt
         CorreoEt = binding.CorreoEt
         ContaseñaEt = binding.ContaseAEt
         ConfirmarContraseñaEt = binding.ConfirmarContraseAEt
@@ -71,6 +103,7 @@ class RegistrarseFragment : Fragment(R.layout.fragment_registrarse) {
         BtnAgregarImagen = binding.agregarImagenBtn
 
         firebaseAuth = FirebaseAuth.getInstance()
+        storage = Firebase.storage
 
         BtnRegistrarUsuario!!.setOnClickListener { ValidarDatos() }
 
@@ -88,6 +121,7 @@ class RegistrarseFragment : Fragment(R.layout.fragment_registrarse) {
 
     private fun ValidarDatos() {
         nombre = NombreEt!!.text.toString()
+        apellido = ApellidoEt!!.text.toString()
         correo = CorreoEt!!.text.toString()
         password = ContaseñaEt!!.text.toString()
         confirmarpassword = ConfirmarContraseñaEt!!.text.toString()
@@ -123,7 +157,21 @@ class RegistrarseFragment : Fragment(R.layout.fragment_registrarse) {
         Datos["uid"] = uid
         Datos["correo"] = correo
         Datos["nombres"] = nombre
+        Datos["apellidos"] = apellido
         Datos["password"] = password
+
+        val storageRef = storage!!.reference
+        val imagenUsuarioRef = storageRef.child("/imagenes/usuarios/$uid/perfil")
+        val stream = FileInputStream(uriImagen?.let { File(it) })
+        var uploadTask = imagenUsuarioRef.putStream(stream)
+
+        uploadTask.addOnFailureListener {
+            Log.e("Error al subir imagen", it.message.toString())
+        }.addOnSuccessListener { taskSnapshot ->
+            Log.e("Exito al subir imagen", taskSnapshot.toString())
+            Datos["imagen"] = taskSnapshot.metadata?.path
+        }
+
         val databaseReference = FirebaseDatabase.getInstance().getReference("Usuarios")
         databaseReference.child(uid!!)
             .setValue(Datos)
@@ -131,7 +179,7 @@ class RegistrarseFragment : Fragment(R.layout.fragment_registrarse) {
                 Toast.makeText(context, "Cuenta creada con éxito", Toast.LENGTH_SHORT).show()
                 findNavController().navigate(R.id.action_navigation_registrarse_to_navigation_home)
             }.addOnFailureListener { e ->
-                Toast.makeText(context, "" + e.message, Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Ocurrio un error al crear cuenta. " + e.message, Toast.LENGTH_SHORT).show()
             }
     }
 
