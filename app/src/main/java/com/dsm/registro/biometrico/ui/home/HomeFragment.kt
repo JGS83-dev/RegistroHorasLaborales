f35b43f128ea1ee67e6f37c79f58c98359207581
birdkron <js8215001@gmail.com>
2024-05-06 18:34:50 -0600

    Agregados permisos de ubicacion, camara

--- app/src/main/java/com/dsm/registro/biometrico/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dsm/registro/biometrico/ui/home/HomeFragment.kt
@@ -1,10 +1,17 @@
 package com.dsm.registro.biometrico.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.dsm.registro.biometrico.R
@@ -29,7 +36,50 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        if (allPermissionsGranted()) {
+            Log.i("Permisos","Se han concedido los permisos")
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
         return root
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                Toast.makeText(context,
+                    "Permisos concedidos por el usuario.",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context,
+                    "Permisos denegados por el usuario.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onDestroyView() {
