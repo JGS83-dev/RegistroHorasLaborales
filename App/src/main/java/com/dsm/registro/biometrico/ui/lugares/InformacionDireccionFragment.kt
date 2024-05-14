1201be6808e02fb41ba04694c975a962d836e6d4
birdkron <js8215001@gmail.com>
2024-05-14 15:53:14 -0600

    ajuste

--- App/src/main/java/com/dsm/registro/biometrico/ui/lugares/InformacionDireccionFragment.kt
+++ App/src/main/java/com/dsm/registro/biometrico/ui/lugares/InformacionDireccionFragment.kt
@@ -302,7 +302,7 @@ class InformacionDireccionFragment : Fragment(R.layout.fragment_informacion_dire
         var distancia = ubicacionActual!!.distanceTo(ubicacionGuardada!!).roundToInt()
         Log.i("Distancia calculada",distancia.toString())
         Log.i("Ubicacion actual",ubicacionActual.toString())
-        Log.i("Operacion",(distancia.compareTo(40)).toString())
+        Log.i("Operacion",(distancia.compareTo(20)).toString())
 
         val calendar = Calendar.getInstance()
         val hour: Int = calendar.get(Calendar.HOUR_OF_DAY)
@@ -310,7 +310,7 @@ class InformacionDireccionFragment : Fragment(R.layout.fragment_informacion_dire
 
         Log.i("Info ID",infoLugar.uid)
 
-        if(distancia.compareTo(40) <= 0){
+        if(distancia.compareTo(20) <= 0){
             val image: InputImage
             var conteoCaras = 0
             try {
